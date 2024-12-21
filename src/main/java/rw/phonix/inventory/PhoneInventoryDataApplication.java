b01410d700c954db0f2687acdec7fc9b1b31e50b
Yvan Gakuba <yvangakuba@Yvans-MacBook-Pro.local>
2024-12-21 15:51:58 -0500

    initialized application

---
+++ src/main/java/rw/phonix/inventory/PhoneInventoryDataApplication.java
@@ -0,0 +1,13 @@
+package rw.phonix.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneInventoryDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneInventoryDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/phonix/inventory/PhoneInventoryDataApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.phonix.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneInventoryDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
